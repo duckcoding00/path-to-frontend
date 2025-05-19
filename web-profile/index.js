a299a76c877517ac5ba7a788e9cb48b94fda34d3
duckcoding <duckcoding00@gmail.com>
2025-05-19 13:55:44 +0700

    web personal

---
+++ web-profile/index.js
@@ -0,0 +1,7 @@
+const burgerIcon = document.querySelector("#nav-burger");
+const menu = document.querySelector("#nav-menu");
+
+burgerIcon.addEventListener("click", () => {
+  burgerIcon.classList.toggle("is-active");
+  menu.classList.toggle("is-active");
+});
